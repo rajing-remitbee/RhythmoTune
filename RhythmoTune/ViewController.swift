8533184a2cb4f9901fda5efa537203add0870530
Rajin Gangadharan <rajin.gangadharan@remitbee.com>
2025-02-24 15:38:23 +0530

    Initial Commit

---
+++ RhythmoTune/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RhythmoTune
+//
+//  Created by Rajin Gangadharan on 24/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RhythmoTuneTests/RhythmoTuneTests.swift
@@ -0,0 +1,17 @@
+//
+//  RhythmoTuneTests.swift
+//  RhythmoTuneTests
+//
+//  Created by Rajin Gangadharan on 24/02/25.
+//
+
+import Testing
+@testable import RhythmoTune
+
+struct RhythmoTuneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
